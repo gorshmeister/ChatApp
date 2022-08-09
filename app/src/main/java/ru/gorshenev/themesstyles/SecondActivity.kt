5447d394d2dca546351e7360170246b62eecaba6
gorshmeister <gorshmeister@gmail.com>
2022-08-09 10:59:35 +0300

    added Bottom Navigation View, removed unnecessary layout components

--- app/src/main/java/ru/gorshenev/themesstyles/SecondActivity.kt
+++ app/src/main/java/ru/gorshenev/themesstyles/SecondActivity.kt
@@ -1,32 +1,44 @@
 package ru.gorshenev.themesstyles
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import ru.gorshenev.themesstyles.fragments.ChannelsFragment
 import ru.gorshenev.themesstyles.fragments.PeopleFragment
 import ru.gorshenev.themesstyles.fragments.ProfileFragment
 
 class SecondActivity : AppCompatActivity() {
+    private val listener = NavigationBarView.OnItemSelectedListener { item ->
+        var selectedFragment: Fragment = ChannelsFragment()
+        when (item.itemId) {
+            R.id.channels -> {
+                selectedFragment = ChannelsFragment()
+            }
+            R.id.people -> {
+                selectedFragment = PeopleFragment()
+            }
+            R.id.profile -> {
+                selectedFragment = ProfileFragment()
+            }
+        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, selectedFragment)
+            .commit()
+        true
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        findViewById<RelativeLayout>(R.id.btn_channels).setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container_view, ChannelsFragment(), )
-                .commit()
-        }
-        findViewById<RelativeLayout>(R.id.btn_people).setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container_view, PeopleFragment(), )
-                .commit()
-        }
-        findViewById<RelativeLayout>(R.id.btn_profile).setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container_view, ProfileFragment(), )
-                .commit()
-        }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener(listener)
 
     }
 }
--- app/src/main/java/ru/gorshenev/themesstyles/fragments/ChannelsFragment.kt
+++ app/src/main/java/ru/gorshenev/themesstyles/fragments/ChannelsFragment.kt
@@ -1,11 +1,15 @@
 package ru.gorshenev.themesstyles.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import ru.gorshenev.themesstyles.MainActivity
 import ru.gorshenev.themesstyles.R
+import ru.gorshenev.themesstyles.SecondActivity
 
 class ChannelsFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +18,14 @@ class ChannelsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_channels, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.tv_channel).setOnClickListener {
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
